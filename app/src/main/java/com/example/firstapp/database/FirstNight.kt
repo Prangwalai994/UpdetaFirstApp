8079d8b6b31894cd823b3432c66593ac3a4be8bd
Wae <pangwalai.li@rmuti.ac.th>
2020-08-15 01:37:35 +0700

    Add Database update

--- app/src/main/java/com/example/firstapp/database/FirstNight.kt
+++ app/src/main/java/com/example/firstapp/database/FirstNight.kt
@@ -1,4 +1,15 @@
 package com.example.firstapp.database
 
-class FirstNight {
-}
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity(tableName = "namecontact_first_quality_table")
+data class FirstNight(
+    @PrimaryKey(autoGenerate = true)
+    var nightId: Long = 0L,
+
+    @ColumnInfo(name = "namecontact_first_quality")
+    val namecontactquality: String = ""
+
+)
--- app/src/main/java/com/example/firstapp/database/FirstappDatabaseDao.kt
+++ app/src/main/java/com/example/firstapp/database/FirstappDatabaseDao.kt
@@ -1,4 +1,28 @@
 package com.example.firstapp.database
 
+import android.arch.lifecycle.LiveData
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
+import android.arch.persistence.room.Update
+
+@Dao
 interface FirstappDatabaseDao {
+    @Insert
+    fun insert(night: FirstNight)
+
+    @Update
+    fun update(night: FirstNight)
+
+    @Query("SELECT * from namecontact_first_quality_table WHERE nightId = :key")
+    fun get(key: Long): FirstNight?
+
+    @Query("DELETE FROM namecontact_first_quality_table")
+    fun clear()
+
+    @Query("SELECT * FROM namecontact_first_quality_table ORDER BY nightId DESC LIMIT 1")
+    fun getTonight(): FirstNight?
+
+    @Query("SELECT * FROM namecontact_first_quality_table ORDER BY nightId DESC")
+    fun getAllNights(): LiveData<List<FirstNight>>
 }
